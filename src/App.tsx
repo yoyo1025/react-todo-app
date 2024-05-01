2a772fec507b4bac81fb945641f21d242a4f39c4
yoyo <hsjejxys@gmail.com>
2024-05-01 13:58:44 +0900

    csrfTokenを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Auth } from './componets/Auth'
 import { Todo } from './componets/Todo'
+import axios from 'axios'
+import { CsrfToken } from './types'
 
 function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
